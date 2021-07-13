1307f02067d1edfb4b45f190b179929ca4643b11
cjuillard <cjuillard@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-07-13 13:25:13 +0000

    corr commit KO

--- src/main/java/fr/ans/sign/esignsantepsc/EsignsantePscApplication.java
+++ src/main/java/fr/ans/sign/esignsantepsc/EsignsantePscApplication.java
@@ -2,8 +2,10 @@ package fr.ans.sign.esignsantepsc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"fr.ans.api.sign.esignsante.psc.api", "fr.ans.api.sign.esignsante.psc.api"})
 public class EsignsantePscApplication {
 
 	public static void main(String[] args) {
