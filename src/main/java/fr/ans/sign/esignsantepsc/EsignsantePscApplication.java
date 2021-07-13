6696f879f2f9ea81b1ce2e666ff351a98fc7be7a
cjuillard <cjuillard@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-07-13 12:39:54 +0000

    Deplacement du projet initial dans le trunk

---
+++ src/main/java/fr/ans/sign/esignsantepsc/EsignsantePscApplication.java
@@ -0,0 +1,13 @@
+package fr.ans.sign.esignsantepsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsignsantePscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsignsantePscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/ans/sign/esignsantepsc/EsignsantePscApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ans.sign.esignsantepsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsignsantePscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
