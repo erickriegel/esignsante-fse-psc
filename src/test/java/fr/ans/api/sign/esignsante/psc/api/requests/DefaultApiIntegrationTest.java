600cab44867230cd1769adc1b9194d2bcba86eb6
cjuillard <cjuillard@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-07-15 13:39:22 +0000

    Ajout 1er restdoc sur test '/'

--- src/test/java/fr/ans/api/sign/esignsante/psc/api/requests/DefaultApiIntegrationTest.java
+++ src/test/java/fr/ans/api/sign/esignsante/psc/api/requests/DefaultApiIntegrationTest.java
@@ -6,19 +6,31 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-//import org.junit.runner.RunWith;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+//import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration
 
 /**
  * Test des EndPoints offerts par l'API esignsante-psc.
  */
+@ExtendWith({RestDocumentationExtension.class, SpringExtension.class}) // pour restdocs
 @ContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -26,6 +38,16 @@ import org.springframework.test.web.servlet.MockMvc;
 
 public class DefaultApiIntegrationTest {
 
+	/*
+	 * setUp pour restdocs
+	 */
+	@BeforeEach
+	public void setUp(WebApplicationContext webApplicationContext,
+	  RestDocumentationContextProvider restDocumentation) {
+	    this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+	      .apply(documentationConfiguration(restDocumentation)).build();
+	}
+	
     /** The mock mvc. */
     @Autowired
     private MockMvc mockMvc;
@@ -40,8 +62,17 @@ public class DefaultApiIntegrationTest {
     @DisplayName("Vérification de la liste des services disponibles.")
     public void rootGetTest() throws Exception {
     	final String body = "[\"/\"]";
-        mockMvc.perform(get("/").accept("application/json"))
-                .andExpect(status().isOk()).andExpect(content().json(body)).andDo(print());
+//        mockMvc.perform(get("/").accept("application/json"))
+//                .andExpect(status().isOk()).andExpect(content().json(body)).andDo(print());
+    	
+    	
+    	  ResultActions returned = mockMvc.perform(get("/").accept("application/json"))
+          .andExpect(status().isOk()).andExpect(content().json(body));
+    	  
+    	  returned.andDo(print()); //pour debug console: a supprimer
+    	  returned.andDo(document("Liste_des_services"));
+          
+    	  
     }
     
 }
