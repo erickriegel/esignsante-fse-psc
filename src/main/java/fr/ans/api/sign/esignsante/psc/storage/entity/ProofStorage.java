a46257b675a9ca1dcf92e3251ab305d504c3532a
cjuillard <cjuillard@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-08-06 13:09:26 +0000

    Correction (recup.) de certains description du yaml

---
+++ src/main/java/fr/ans/api/sign/esignsante/psc/storage/entity/ProofStorage.java
@@ -0,0 +1,42 @@
+package fr.ans.api.sign.esignsante.psc.storage.entity;
+
+import java.util.Date;
+
+import org.bson.BsonDocument;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+//@EqualsAndHashCode(of= {"name","request"})
+@ToString(of= {"id","subjectOrganization","preferred_username","given_name","family_name","timestamp"})
+@Getter
+@Document(collection = "archives")
+public class ProofStorage {
+	@Id
+    private String id;
+	
+	@Setter
+    private String subjectOrganization;
+	
+	@Setter
+    private String preferred_username;
+	
+	@Setter
+    private String given_name;
+	
+	@Setter
+    private String family_name;
+	
+	@Setter
+	@CreatedDate
+    private Date timestamp;
+	
+	@Setter
+    private BsonDocument bsonProof;
+	
+	
+}
