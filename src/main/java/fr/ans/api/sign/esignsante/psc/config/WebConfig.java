617737b78a78b1401309280d25ff8777cad365d2
pmortas <pmortas@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-09-14 20:44:14 +0000

    Surcharge des info de l'API

--- src/main/java/fr/ans/api/sign/esignsante/psc/config/WebConfig.java
+++ src/main/java/fr/ans/api/sign/esignsante/psc/config/WebConfig.java
@@ -3,12 +3,16 @@
  */
 package fr.ans.api.sign.esignsante.psc.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -21,14 +25,29 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class WebConfig {
 
+	/** ESignSante Build Properties. */
+	@Autowired
+	private BuildProperties buildProperties;
+
+	ApiInfo apiInfo() {
+		return new ApiInfoBuilder()
+				.title("EsignSante-psc V " + buildProperties.getVersion())
+				.description("Api de signature à partir d'un jeton ProSantéConnect")
+				.license("Apache 2.0")
+				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+				.termsOfServiceUrl("")
+				.version("1.0")
+				.build();
+	}
+
 	@Bean
 	public Docket api() {
 		return new Docket(DocumentationType.SWAGGER_2)
-				.useDefaultResponseMessages(false).select()
+				.useDefaultResponseMessages(false)
+				.select()
 				.apis(RequestHandlerSelectors.basePackage("fr.ans.api.sign.esignsante.psc.api"))
-				.paths(PathSelectors.any())
-				.build();
-		
+				.paths(PathSelectors.any()).build();
+
 	}
 
 }
