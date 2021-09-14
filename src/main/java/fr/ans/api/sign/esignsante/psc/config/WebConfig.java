36196b53989ed395ae9d1690d8f85a42b5b01a7f
pmortas <pmortas@4cfcd01c-db38-48b1-8200-c3c7a2811bfb>
2021-09-14 21:17:02 +0000

    Surcharge des info de l'API

--- src/main/java/fr/ans/api/sign/esignsante/psc/config/WebConfig.java
+++ src/main/java/fr/ans/api/sign/esignsante/psc/config/WebConfig.java
@@ -13,6 +13,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -35,8 +36,9 @@ public class WebConfig {
 				.description("Api de signature à partir d'un jeton ProSantéConnect")
 				.license("Apache 2.0")
 				.licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-				.termsOfServiceUrl("")
+				.termsOfServiceUrl("https://esante.gouv.fr")
 				.version("1.0")
+				.contact(new Contact("ANS", "https://esante.gouv.fr", "esignsante@esante.gouv.fr"))
 				.build();
 	}
 
